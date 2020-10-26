e41e2f6cce40fd94a6bf2cdf20a29e12a74782c9
Iron Maid <68044807+Sullag@users.noreply.github.com>
2020-10-26 19:23:03 +0100

    Add a const.cpp file
    Another one lesson for me

---
+++ const.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	const int DAYS_IN_WEEK = 7; // Объявляем константу(переменная, которая не меняется) обозначающая колличество дней в неделе
+	cout << DAYS_IN_WEEK << endl; // Проверка. Кстати да, объявляй константу капсом. Так надо
+	const char NEW_LINE = '\n'; // Ебать, а так можно было?
+	cout << NEW_LINE << DAYS_IN_WEEK << NEW_LINE; // Проверили. Работает.
+	const char TAB = '\t'; // И так тоже можно
+	cout << TAB << TAB << "Hello"; // Проверка
+}
